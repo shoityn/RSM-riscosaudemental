40c3df819e78f63f70fadce2578278284725c226
shoityn <135985931+shoityn@users.noreply.github.com>
2025-02-25 21:31:58 -0300

    Update firebaseUtils.ts

--- src/app/lib/firebaseUtils.ts
+++ src/app/lib/firebaseUtils.ts
@@ -1,26 +1,33 @@
-import { doc, setDoc, updateDoc } from "firebase/firestore";
+import { doc, updateDoc, getDoc, arrayUnion, setDoc } from "firebase/firestore";
 import { db } from "../firebaseConfig";
 
 export const salvarResultado = async (cns: string, tipo: string, valor: number, risco: string) => {
   try {
     const docRef = doc(db, "resultados", cns);
+    const docSnap = await getDoc(docRef);
 
-    const dados = {
-      CNS: cns,
-      resultados: {
-        [tipo]: {
-          pontuacao: valor,
-          risco: risco,
-        },
-      },
-      
+    const novoResultado = {
+      pontuacao: valor,
+      risco: risco,
       data: new Date(),
     };
 
-    await setDoc(docRef, dados, { merge: true });
-
-    console.log("Resultado salvo com sucesso no documento do usuário:", cns);
+    if (docSnap.exists()) {
+      // O documento já existe, então adicionamos o novo teste ao array dentro do tipo correspondente
+      await updateDoc(docRef, {
+        [`resultados.${tipo}`]: arrayUnion(novoResultado),
+      });
+    } else {
+      // Criamos um novo documento com o primeiro resultado dentro de um array
+      await setDoc(docRef, {
+        CNS: cns,
+        data: new Date(),
+        resultados: {
+          [tipo]: [novoResultado], // Inicializa com um array
+        },
+      });
+    }
   } catch (e) {
     console.error("Erro ao salvar resultado:", e);
   }
-}
+};
